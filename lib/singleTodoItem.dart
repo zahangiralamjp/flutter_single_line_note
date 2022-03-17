5a8f9a628d6570b5454ee95a12abbe413839b8b7
ZahangirAlamJP <thezahangir@gmail.com>
2022-03-17 07:09:07 -0700

    Complate ! Simple DO TO

--- lib/singleTodoItem.dart
+++ lib/singleTodoItem.dart
@@ -1,27 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_todo/home.dart';
-
+import 'package:intl/intl.dart';
 class SingleTodoItem extends StatelessWidget {
   final int? index;
   final String? id;
   final String? title;
-  final DateTime? dateTime;
-  final Function delatetod;
-
-  SingleTodoItem({this.index, this.id, this.title, this.dateTime,required this.delatetod});
-
+  final DateTime? datetime;
+  final Function? delatetod;
+  SingleTodoItem(
+      {this.index, this.id, this.title, this.datetime, this.delatetod});
   @override
   Widget build(BuildContext context) {
-    delatetod;
-    return Card(child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-      Text("$index"),
-      Text(" $id "),
-      Text(" $title "),
-     
-      //Spacer(),
-      IconButton(onPressed: (){
-      delatetod(id);
-      }, icon: Icon(Icons.delete)),
-    ],),);
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 10,
+          vertical: 5,
+        ),
+        child: Row(
+          children: [
+            Text(
+              "$index",
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(width: 20),
+            Text(
+              "$title",
+              style: TextStyle(
+                fontSize: 22,
+              ),
+            ),
+            SizedBox(width: 10),
+            Text(
+              datetime != null
+                  ? "${DateFormat('dd-mm-yyy').format(datetime!)}"
+                  : "No Date",
+            ),
+            Spacer(),
+            IconButton(
+              onPressed: () {
+                delatetod!(id);
+              },
+              icon: Icon(
+                Icons.delete,
+                size: 25,
+                color: Colors.red,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
